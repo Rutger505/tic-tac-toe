e0f30a54e60891f1a14929afd6a6e65733fb67b6
Rutger <rutger.pronk11@gmail.com>
2024-05-23 23:43:19 +0200

    Start creating leaderboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Leaderboard from "@/components/homepage/leaderboard";
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
           </Button>
         </div>
       </div>
+      <Leaderboard />
     </main>
   );
 }
