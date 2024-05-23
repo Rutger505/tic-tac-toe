86909332edb38e0727cba6eb82359689a8d17868
Rutger <rutger.pronk11@gmail.com>
2024-05-23 23:19:14 +0200

    Add header, add pretier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>Tic-tac-toe</h1>
         </main>
-
-
     );
 }
 
