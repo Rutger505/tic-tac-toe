69cd469910cb76df50005e604f90dee2d6bf529b
Rutger <rutger.pronk11@gmail.com>
2024-05-23 23:37:40 +0200

    Create left half of homepage with shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
-export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>Tic-tac-toe</h1>
-        </main>
-    );
-}
+import { Button } from "@/components/ui/button";
 
+export default function Home() {
+  return (
+    <main className="flex p-56">
+      <div className={"flex flex-col gap-28 justify-center -mt-36"}>
+        <h1 className={"font-bold text-7xl"}>Tic-tac-toe</h1>
+        <div className={"flex gap-8"}>
+          <Button className={"rounded-md px-4 h-14 text-2xl font-medium"}>
+            Quick play
+          </Button>
+          <Button
+            variant={"outline"}
+            className={"rounded-md px-4 h-14 text-2xl font-medium"}
+          >
+            Play with friend
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
+}
