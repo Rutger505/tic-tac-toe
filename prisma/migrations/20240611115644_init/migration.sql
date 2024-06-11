2334779c7f6836d1eaf873b722305c66a3ec3397
Rutger <1201954@student.roc-nijmegen.nl>
2024-06-11 13:57:29 +0200

    Got prisma adapter working by removing middleware that runs on edge runtime

--- prisma/migrations/20240611115644_init/migration.sql
+++ prisma/migrations/20240611115644_init/migration.sql
@@ -5,6 +5,7 @@ CREATE TABLE "User" (
     "email" TEXT,
     "emailVerified" DATETIME,
     "image" TEXT,
+    "role" TEXT,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth";
 import Google from "@auth/core/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "@/lib/prisma";
+import db from "@/lib/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(db),
   providers: [Google],
   debug: process.env.NODE_ENV === "development",
 });
