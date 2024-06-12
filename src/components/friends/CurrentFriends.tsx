37783aa997836529f1ebd83546755c5cd7934b3b
Rutger <1201954@student.roc-nijmegen.nl>
2024-06-12 10:29:20 +0200

    Add current friend logic

--- src/components/friends/CurrentFriends.tsx
+++ src/components/friends/CurrentFriends.tsx
@@ -1,24 +1,38 @@
-"use client";
-
-import UserList from "@/components/friends/UserList";
+import { auth } from "../../auth";
+import { redirect } from "next/navigation";
+import db from "@/lib/db";
 import { TrashIcon } from "@/components/icons/TrashIcon";
+import UserList from "@/components/friends/UserList";
 
-export function CurrentFriends() {
-  const users = [
-    {
-      id: "idesd",
-      name: "RutgerDeN00b",
-      email: "jogn@email.com",
+export async function CurrentFriends() {
+  const session = await auth();
+  if (!session) {
+    return redirect("/api/auth/signin");
+  }
+
+  const friendships = await db.friendship.findMany({
+    where: {
+      OR: [
+        { user1Id: session.session.user.id, status: "ACCEPTED" },
+        { user2Id: session.session.user.id, status: "ACCEPTED" },
+      ],
     },
-    {
-      id: "idasdflkjasdf;lkasdfesd",
-      name: "John Doe",
-      email: "asfsadf@asd.com",
+    include: {
+      user1: true,
+      user2: true,
     },
-  ];
+  });
+
+  const friends = friendships.map((friendship) => {
+    if (friendship.user1Id === session.session.user.id) {
+      return friendship.user2;
+    }
+    return friendship.user1;
+  });
+
   return (
     <UserList
-      users={users}
+      users={friends}
       actionCell={(user) => (
         <button>
           <TrashIcon className={"h-4 -mb-1"} />
