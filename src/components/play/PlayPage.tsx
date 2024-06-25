4b4377afed00f88b67023cc89da9310c6fb61d2e
Rutger <1201954@student.roc-nijmegen.nl>
2024-06-25 17:05:13 +0200

    Set board in PlayPage component

--- src/components/play/PlayPage.tsx
+++ src/components/play/PlayPage.tsx
@@ -5,17 +5,47 @@ import { io } from "socket.io-client";
 
 export default function PlayPage() {
   useEffect(() => {
-    // Initialize the WebSocket connection
     const socket = io(process.env.NEXT_PUBLIC_WEBSOCKET_URL as string);
 
-    // Emit join-queue event
     socket.emit("join-queue");
 
-    // Cleanup function to disconnect the socket when the component is unmounted
     return () => {
       socket.disconnect();
     };
-  }, []); // Empty dependency array ensures this runs only once
+  }, []);
 
-  return "Test";
+  return (
+    <main className={"mx-auto"}>
+      <div className={"space-y-10"}>
+        <h1 className={"font-bold text-3xl text-center"}>
+          Searchng for opponent
+        </h1>
+        <div className={`relative grid grid-cols-3 grid-rows-3 h-96 w-96`}>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+
+          {/* Rounded lines */}
+          <div className="absolute inset-0 flex flex-col justify-evenly -z-10">
+            <div className="w-full h-[6px] bg-black rounded-full"></div>
+            <div className="w-full h-[6px] bg-black rounded-full"></div>
+          </div>
+          <div className="absolute inset-0 flex justify-evenly -z-10">
+            <div className="w-[6px] h-full bg-black rounded-full"></div>
+            <div className="w-[6px] h-full bg-black rounded-full"></div>
+          </div>
+        </div>
+
+        <p className={"text-center text-lg font-medium"}>
+          Opponent: Searching...
+        </p>
+      </div>
+    </main>
+  );
 }
