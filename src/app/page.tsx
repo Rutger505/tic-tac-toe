bbbafeda6e5229e24e671992fa2c6bc6dd0666eb
Rutger <1201954@student.roc-nijmegen.nl>
2024-06-25 16:59:40 +0200

    Login before going to play page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 import { Button } from "@/components/ui/button";
 import Leaderboard from "@/components/homepage/leaderboard";
+import Link from "next/link";
+import { auth } from "@/auth";
 
 export default async function Home() {
+  const session = await auth();
+
   return (
     <main className="grid grid-cols-2">
       <div className={"flex flex-col gap-28 mt-48 mx-auto"}>
         <h1 className={"font-bold text-6xl"}>Tic-tac-toe</h1>
         <div className={"flex gap-8"}>
           <Button className={"rounded-md px-4 h-14 text-2xl font-medium"}>
-            Quick play
+            <Link href={session ? "/play" : "/api/auth/signin"}>
+              Quick play
+            </Link>
           </Button>
           <Button
             variant={"outline"}
