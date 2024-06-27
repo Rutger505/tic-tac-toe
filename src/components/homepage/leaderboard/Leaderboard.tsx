d86464be581836df39496380106b9897f30c2069
Rutger <rutger.pronk11@gmail.com>
2024-06-27 20:20:27 +0200

    Round win persentage to one floating point, do logic outside of "html"

--- src/components/homepage/leaderboard/Leaderboard.tsx
+++ src/components/homepage/leaderboard/Leaderboard.tsx
@@ -53,7 +53,9 @@ function getUserWithStats(
 
   const wins = gamesInPeriod.filter((game) => game.winnerId === user.id).length;
   const winPercentage =
-    gamesInPeriod.length === 0 ? 0 : (wins / gamesInPeriod.length) * 100;
+    gamesInPeriod.length === 0
+      ? 0
+      : Math.round((wins / gamesInPeriod.length) * 1000) / 10;
 
   return {
     ...user,
@@ -81,7 +83,7 @@ export default async function Leaderboard() {
   const currentUser = session
     ? await db.user.findFirst({
         where: {
-          id: session.session.user.id,
+          id: session.session?.user.id,
         },
         select: {
           id: true,
@@ -93,13 +95,6 @@ export default async function Leaderboard() {
       })
     : null;
 
-  const currentUserDailyStats =
-    currentUser && getUserWithStats(currentUser, "daily");
-  const currentUserWeeklyStats =
-    currentUser && getUserWithStats(currentUser, "weekly");
-  const currentUserAllTimeStats =
-    currentUser && getUserWithStats(currentUser, "all-time");
-
   const top10DailyUsers = users
     .map((user) => getUserWithStats(user, "daily"))
     .slice(0, 10);
@@ -109,6 +104,21 @@ export default async function Leaderboard() {
   const top10AllTimeUsers = users
     .map((user) => getUserWithStats(user, "all-time"))
     .slice(0, 10);
+
+  const currentUserDailyStats =
+    currentUser && !top10DailyUsers.some((user) => user.id === currentUser.id)
+      ? getUserWithStats(currentUser, "daily")
+      : null;
+
+  const currentUserWeeklyStats =
+    currentUser && !top10WeeklyUsers.some((user) => user.id === currentUser.id)
+      ? getUserWithStats(currentUser, "weekly")
+      : null;
+
+  const currentUserAllTimeStats =
+    currentUser && !top10AllTimeUsers.some((user) => user.id === currentUser.id)
+      ? getUserWithStats(currentUser, "all-time")
+      : null;
 
   return (
     <div className={"mx-auto mt-16"}>
@@ -133,40 +143,19 @@ export default async function Leaderboard() {
         <TabsContent value="daily" className={"max-h-80 overflow-y-auto"}>
           <LeaderboardList
             users={top10DailyUsers}
-            currentUser={
-              currentUserDailyStats &&
-              !top10DailyUsers.some(
-                (user) => user.id === currentUserDailyStats.id,
-              )
-                ? currentUserDailyStats
-                : null
-            }
+            currentUser={currentUserDailyStats}
           />
         </TabsContent>
         <TabsContent value="weekly" className={"max-h-80 overflow-y-auto"}>
           <LeaderboardList
             users={top10WeeklyUsers}
-            currentUser={
-              currentUserWeeklyStats &&
-              !top10WeeklyUsers.some(
-                (user) => user.id === currentUserWeeklyStats.id,
-              )
-                ? currentUserWeeklyStats
-                : null
-            }
+            currentUser={currentUserWeeklyStats}
           />
         </TabsContent>
         <TabsContent value="all-time" className={"max-h-80 overflow-y-auto"}>
           <LeaderboardList
             users={top10AllTimeUsers}
-            currentUser={
-              currentUserAllTimeStats &&
-              !top10AllTimeUsers.some(
-                (user) => user.id === currentUserAllTimeStats.id,
-              )
-                ? currentUserAllTimeStats
-                : null
-            }
+            currentUser={currentUserAllTimeStats}
           />
         </TabsContent>
       </Tabs>
