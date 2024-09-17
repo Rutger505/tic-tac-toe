b71a6b4cd0c50d6ed11497118af26ddbcf17b57e
Rutger <rutger.pronk11@gmail.com>
2024-09-17 23:46:34 +0200

    Add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,33 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReactNode, StrictMode } from "react";
 import Header from "@/components/Header";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  metadataBase: process.env.BASE_URL,
+  title: "Tic-tac-toe – Multiplayer",
+  description: "Play tic-tac-toe with your friends online!",
+  keywords: [
+    "Rutger Pronk",
+    "Rutger",
+    "Pronk",
+    "tic-tac-toe",
+    "tic-tac-toe multiplayer",
+    "multiplayer",
+    "online",
+    "game",
+  ],
+  openGraph: {
+    title: "Tic-tac-toe – Multiplayer",
+    description: "Play tic-tac-toe with your friends online!",
+    siteName: "Tic-tac-toe – Multiplayer",
+    type: "website",
+    locale: "en",
+    url: process.env.BASE_URL,
+  },
+};
 
 export default function RootLayout({
   children,
