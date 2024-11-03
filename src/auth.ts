2264a3249d3e0781e8529d6d1437c817598b5e37
Rutger <rutger.pronk11@gmail.com>
2024-11-03 22:16:27 +0100

    When not in production use credentials provider which supports every login

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,8 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import db from "@/lib/db";
 import NextAuth, { type DefaultSession } from "next-auth";
 import { User } from "@/types/user";
+import Credentials from "@auth/core/providers/credentials";
+import { createHash } from "@auth/core/lib/utils/web";
 
 declare module "next-auth" {
   /**
@@ -16,15 +18,33 @@ declare module "next-auth" {
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(db),
   providers: [
-    Google({
-      profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-        };
-      },
-    }),
+    process.env.NODE_ENV === "production"
+      ? Google({
+          profile(profile) {
+            return {
+              id: profile.sub,
+              name: profile.name,
+              email: profile.email,
+              image: profile.picture,
+            };
+          },
+        })
+      : Credentials({
+          name: "Development",
+          credentials: {
+            username: { label: "Username", type: "text", required: true },
+            password: { label: "Password", type: "password", required: true },
+          },
+          async authorize(credentials) {
+            const id = await createHash(credentials.username);
+
+            return {
+              id: id,
+              name: `${credentials.username}`,
+              email: `${credentials.username}@example.com`,
+              image: "https://placehold.co/64x64",
+            };
+          },
+        }),
   ],
 });
